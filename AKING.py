cb925a5237d3556f082600617df58605593d667f
hstap <106961667+hstap@users.noreply.github.com>
2022-09-15 17:14:24 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -5,7 +5,6 @@ os.system('git pull')
 bit = platform.architecture()[0]
 if bit=='64bit':
     print('\033[1;32m[•] Congrats! Your Device Support This Tools \033[1;37m')
-    os.system('xdg-open https://facebook.com/groups/351076900316263/')
     import AKING
 else:
     exit('\033[1;31m[×] Sorry Device Not Support')
